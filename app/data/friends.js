68aaeab142c0678867771ffc6efb5cd81fba8ae0
Steven Dytewski <40008730+Dy2ski@users.noreply.github.com>
2019-04-01 17:55:54 -0700

    tried my best

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+// In this code below we create the Front-end JavaScript which "POSTS" our form data to our express server.
+// In essence, when the user hits submit, jQuery grabs all of the fields then sends a post request to our api
+// Our api recognizes the route (/api/tables)... and then runs the associated code (found in api-routes.js).
+// In this case the associated code "saves" the data to the table-data.js file or waitinglist-data.js file
+var arrayFriends = [
+
+{
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+]
+
+module.exports = arrayFriends;
